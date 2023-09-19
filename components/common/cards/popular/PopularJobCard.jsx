3992c99c6d2ff0a8c11b2d5c4536fd30f00037b6
Kanya Krishi <kanyakrishi@gmail.com>
2023-09-19 01:30:46 -0500

    Home page done

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
+import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
@@ -10,8 +11,24 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
       onPress={() => handleCardPress(item)}
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-        <Image source={{ uri: item.employer_logo }} style={styles.logoImage} />
+        <Image
+          source={{
+            uri: checkImageURL(item.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+        />
       </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+      </View>
+      <Text style={styles.location}>{item.job_country}</Text>
     </TouchableOpacity>
   );
 };
@@ -19,4 +36,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
 // we have to make each of the card clikcable - hence within the touchable opacity add this
 // This is an event handler for the touch event. When the <TouchableOpacity> component is pressed, the handleCardPress function is called with the current item as its argument. The handleCardPress function would contain logic to determine what should happen when the component is pressed
 
+// number of lines = 1 ==> only 1 line will be visible on the card. Rest will be ... 
+
+// defaul image - check if image is there or not --> use this code. Tests it and gives 200 if it exists.
 export default PopularJobCard;
